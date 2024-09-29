7a86c692b92ebf4bc244ee7f63db4f51e8703792
Marta Gómez <martagomezmartinez95@gmail.com>
2024-09-29 12:58:06 +0200

    Solved lab

--- src/functions-and-arrays.js
+++ src/functions-and-arrays.js
@@ -1,34 +1,76 @@
 // Iteration 1 | Find the Maximum
-function maxOfTwoNumbers() {}
-
-
-
+function maxOfTwoNumbers(a, b) {
+  if (a >= b) {
+    return a;
+  } else {
+    return b;
+  }
+}
 
 // Iteration 2 | Find the Longest Word
-const words = ["mystery", "brother", "aviator", "crocodile", "pearl", "orchard", "crackpot"];
+const words = [
+  "mystery",
+  "brother",
+  "aviator",
+  "crocodile",
+  "pearl",
+  "orchard",
+  "crackpot",
+];
 
-function findLongestWord() {}
-
-
-
+function findLongestWord(arrayWords) {
+  if (arrayWords.length === 0) {
+    return null;
+  }
+  let longestWord = "";
+  for (let i = 0; i < arrayWords.length; i++) {
+    if (arrayWords[i].length > longestWord.length) {
+      longestWord = arrayWords[i];
+    }
+  }
+  return longestWord;
+}
 
 // Iteration 3 | Sum Numbers
 const numbers = [6, 12, 1, 18, 13, 16, 2, 1, 8, 10];
 
-function sumNumbers() {}
-
-
-
+function sumNumbers(arrayNumbers) {
+  let sum = 0;
+  for (let i = 0; i < arrayNumbers.length; i++) {
+    sum += arrayNumbers[i];
+  }
+  return sum;
+}
 
 // Iteration 4 | Numbers Average
 const numbers2 = [2, 6, 9, 10, 7, 4, 1, 9];
 
-function averageNumbers() {}
-
-
-
+function averageNumbers(arrayNumbersTwo) {
+    const sum = sumNumbers(arrayNumbersTwo);
+    const elements = arrayNumbersTwo.length;
+    if(arrayNumbersTwo.length=== 0){
+        return 0;
+    }
+    const average = sum/elements;
+    return average;
+}
 
 // Iteration 5 | Find Elements
-const words2 = ["machine", "subset", "trouble", "starting", "matter", "eating", "truth", "disobedience"];
+const words2 = [
+  "machine",
+  "subset",
+  "trouble",
+  "starting",
+  "matter",
+  "eating",
+  "truth",
+  "disobedience",
+];
 
-function doesWordExist() {}
+function doesWordExist(arrayWordsTwo,wordToSearch) {
+    if (arrayWordsTwo.length===0){
+        return null;
+    } else{
+        return arrayWordsTwo.includes(wordToSearch);
+    }
+}
